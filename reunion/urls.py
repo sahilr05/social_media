af529ef6365c2cfd74f8c439dc259cf7c35b9894
Sahil Rajpal <sahilrajpal85@gmail.com>
2022-10-02 21:01:43 +0530

    move auth URLs to social_app

--- reunion/urls.py
+++ reunion/urls.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path(
-        "auth/token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
-    ),
-    path(
-        "auth/token/refresh/",
-        jwt_views.TokenRefreshView.as_view(),
-        name="token_refresh",
-    ),
     path("admin/", admin.site.urls),
     path("api/", include("social_app.urls")),
 ]
--- social_app/urls.py
+++ social_app/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from social_app import views
-
 
 app_name = "social_app"
 urlpatterns = []
 
 user_urls = [
     # path("test/", views.TestApi().as_view(), name="test_api"),
+    path(
+        "authentication/",
+        jwt_views.TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
     path("user/", views.UserDetailAPI().as_view(), name="user_detail"),
     path("user/sign_up/", views.UserSignUpAPI().as_view(), name="user_sign_up"),
 ]
