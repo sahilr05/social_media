92a3f11710d63e53f1fc886d4c8e42f5c8f4e525
Sahil Rajpal <sahilrajpal85@gmail.com>
2022-10-03 00:30:19 +0530

    add test script in start-server sh

--- start-server.sh
+++ start-server.sh
@@ -2,11 +2,14 @@
 # start-server.sh
 
 # Apply database migrations
-echo "Apply database migrations"
+echo "Apply database migrations.."
 python manage.py migrate
 
 if [[ "$DOMAIN" == "dev" ]]
 then
+  echo "Running tests.."
+  python manage.py test social_app.tests
+  echo "Starting server.."
   python manage.py runserver 0.0.0.0:8000
 else
   gunicorn reunion.wsgi:application --bind :8000
