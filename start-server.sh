3628e9912ab409cfb245126c282fdc6db602d0a5
Sahil Rajpal <sahilrajpal85@gmail.com>
2022-10-03 23:29:01 +0530

    add db check in start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -2,8 +2,21 @@
 # start-server.sh
 
 # Apply database migrations
-echo "Apply database migrations.."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
 python manage.py migrate
+
+exec "$@"
 
 if [[ "$DOMAIN" == "dev" ]]
 then
